2028622d94b5eece43847bdc882b924c1ff5e9f4
Ubuntu <ubuntu@ip-172-31-29-76.ec2.internal>
2021-11-12 11:20:18 +0000

    updated code

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -7,22 +7,28 @@ class Dec2Hex
 
     public static void main(String args[])
     {
-        Arg1 = Integer.parseInt(args[0]);
-        char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-        int rem, num;
-        num = Arg1;
-        String hexadecimal="";
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
-        while(num != 0)
+        try
         {
-            rem=num%16;
-            hexadecimal= ch[rem] + hexadecimal;
-            num= num/16;
+                Arg1 = Integer.parseInt(args[0]);
+                char ch[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
+                int input, rem, num;
+                num = Arg1;
+                input = num;
+                String hexadecimal="";
+
+                while(num != 0)
+                {
+                rem=num%16;
+                hexadecimal= ch[rem] + hexadecimal;
+                num= num/16;
+                }
+
+                System.out.println("Hexadecimal representation of " + input + " is " + hexadecimal);
         }
-
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
-
+        catch (Exception ex)
+        {
+                System.out.println("Error : Enter Integer Value");
+        }
     }
 }
 
